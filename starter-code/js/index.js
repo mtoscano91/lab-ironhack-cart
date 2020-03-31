50107f56b7f178052c8cc72f85fafc0d23e121de
Jan Redmann <janredmann1@gmail.com>
2020-03-31 11:15:05 +0200

    initial commit

---
+++ starter-code/js/index.js
@@ -0,0 +1,11 @@
+var $cart = document.querySelector('#cart tbody');
+var $calc = document.getElementById('calc');
+
+function updateSubtotal($product) {
+  // Iteration 1.1
+}
+
+function calculateAll() {
+  // Iteration 1.2
+}
+$calc.onclick = calculateAll;
